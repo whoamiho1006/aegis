0f779d082d93b0fcf7b0667bfe077c6dec2aadc8
Jay K <33371449+jay94ks@users.noreply.github.com>
2020-08-20 11:59:26 +0900

    HTTP Context and its component base.
    Few implementations are done. however more implementations are needed.
    
    Co-Authored-By: tjrdud9665 <16893042+tjrdud9665@users.noreply.github.com>
    Co-Authored-By: Jay K <33371449+jay94ks@users.noreply.github.com>

---
+++ src/Aegis.Core/Endpoints/HTTP/Context.Component.cs
@@ -0,0 +1,47 @@
+﻿namespace Aegis.Endpoints.HTTP
+{
+    public partial class Context
+    {
+        /// <summary>
+        /// Context component.
+        /// </summary>
+        public class Component
+        {
+            /// <summary>
+            /// Begins this component with context.
+            /// </summary>
+            /// <param name="Context"></param>
+            internal void Begin(Context Context)
+            {
+                this.Context = Context;
+                OnBegin();
+            }
+
+            /// <summary>
+            /// Ends this component and removed from context.
+            /// </summary>
+            internal void End()
+            {
+                OnEnd();
+                Context = null;
+            }
+
+            /// <summary>
+            /// Context reference.
+            /// </summary>
+            public Context Context { get; private set; }
+
+            /// <summary>
+            /// Callback for begining component.
+            /// </summary>
+            protected virtual void OnBegin() { }
+
+            /// <summary>
+            /// Callback for ending component.
+            /// </summary>
+            protected virtual void OnEnd() { }
+        }
+
+        
+    }
+}
---
+++ src/Aegis.Core/Endpoints/HTTP/Context.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aegis.Endpoints.HTTP
+{
+    public partial class Context
+    {
+        private List<Component> m_Components;
+
+        /// <summary>
+        /// Initialize a context with request and response.
+        /// </summary>
+        /// <param name="Request"></param>
+        /// <param name="Response"></param>
+        public Context(IRequest Request, IResponse Response)
+        {
+            m_Components = new List<Component>();
+
+            this.Request = Request;
+            this.Response = Response;
+        }
+
+        /// <summary>
+        /// Request and its body of this context.
+        /// </summary>
+        public IRequest Request { get; }
+
+        /// <summary>
+        /// Response and its body for this context.
+        /// </summary>
+        public IResponse Response { get; }
+
+
+    }
+}
